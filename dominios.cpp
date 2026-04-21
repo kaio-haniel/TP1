ead31debcbc052a18914115c84f3c0cc769e0222
DaniloH <194696717+HDaniloH@users.noreply.github.com>
2026-04-21 15:57:32 -0300

    Update dominios.cpp

--- dominios.cpp
+++ dominios.cpp
@@ -5,86 +5,86 @@
 using namespace std;
 
 //Metodos-Codigo:
-bool Codigo::validar(string code){
-    if(code.size()!=LIMITE) return false;
-    if(!isupper(code[0]) || !isupper(code[1])) return false;
-    if(!isdigit(code[2]) || !isdigit(code[3]) || !isdigit(code[4])) return false;
-    return true;
+void Codigo::validar(string code){
+    if(code.size()!=LIMITE)
+        throw invalid_argument(to_string(code.size()) + " eh um tamanho invalido!"
+                               + " Eh esperado " + to_string(LIMITE) + " caracteres.");
+    if(!isupper(code[0]) || !isupper(code[1]))
+        throw invalid_argument("Argumento invalido. Os dois primeiros caracteres devem ter o formato 'XX' (letras maiusculas).");
+    if(!isdigit(code[2]) || !isdigit(code[3]) || !isdigit(code[4]))
+        throw invalid_argument("Argumento invalido. Eh esperado 3 digitos numericos finais.");
 }
-bool Codigo::setCode(string code){
-    if(!validar(code)) return false;
+void Codigo::setCode(string code){
+    validar(code);
     this->Code = code;
-    return true;
 }
 
 //Metodos-Senha:
-bool Senha::validar(string senha){
+void Senha::validar(string senha){
 
-    if(senha.size()!=LIMITE) return false;
+    if(senha.size()!=LIMITE)
+        throw invalid_argument("Senha invalida: " + to_string(senha.size()) + " eh um tamanho invalido!"
+                               + " Eh esperado " + to_string(LIMITE) + " caracteres.");;
 
     int count_AZ=0, count_az=0, count_dig=0;
 
     for(int i=0; i<LIMITE; i++){
 
-        if(!isalnum(senha[i])) return false;
+        if(!isalnum(senha[i])) throw invalid_argument("Caracter pode apenas ser letra (A-Z ou a-z) ou digito (0-9).");
 
         if(i<senha.size()-1){
             if((isalpha(senha[i])&&isalpha(senha[i+1]))||
                (isdigit(senha[i])&&isdigit(senha[i+1]))){
-                return false;
+                throw invalid_argument("Letra nao pode ser seguida por letra, digito nao pode ser seguido por digito.");
             }
         }
         if(isupper(senha[i])) count_AZ++;
         else if(islower(senha[i])) count_az++;
         else if(isdigit(senha[i])) count_dig++;
     }
-    if(count_AZ == 0 || count_az == 0 || count_dig == 0) return false;
-
-    return true;
+    if(count_AZ == 0 || count_az == 0 || count_dig == 0)
+        throw invalid_argument("Devem existir pelo menos uma letra minuscula (a-z), uma letra maiuscula (A-Z) e um digito (0-9).");
 }
-bool Senha::setSenha(string senha){
-    if(!validar(senha)) return false;
+void Senha::setSenha(string senha){
+    validar(senha);
     this->senha = senha;
-    return true;
 }
 
 //Metodos-Tempo:
-bool Tempo::validar(int n){
-    if(n > min_t && n <= max_t) return true;
-    return false;
+void Tempo::validar(int n){
+    if(!(n > min_t && n <= max_t)) throw invalid_argument("Argumento invalido. Insira um digito entre 1 e 365.");
 }
-bool Tempo::setDuracao(int tempo){
-    if(!validar(tempo)) return false;
+void Tempo::setDuracao(int tempo){
+    validar(tempo);
     this->duracao = tempo;
-    return true;
 }
 
 //Metodos-Texto:
-bool Texto::validar(string texto){
+void Texto::validar(string texto){
 
     int tam = texto.size();
 
-    if(tam>LIMITE) return false;
+    if(tam>LIMITE) throw invalid_argument("Argumento invalido. O tamanho do texto pode ser de no maximo " + to_string(LIMITE) + " caracteres.");
 
-    if(!isalnum(texto[0]) || !isalnum(texto[tam-1])) return false;
+    if(!isalnum(texto[0]) || !isalnum(texto[tam-1]))
+        throw invalid_argument("O primeiro e o ultimo caracteres so podem ser letras (a-z ou A-Z) ou digitos (0-9).");
 
     for(int i = 0; i < tam; i++){
 
         if(!isalnum(texto[i]) && texto[i] != ',' && texto[i] != '.' && texto[i] != ' ')
-            return false;
+            throw invalid_argument("Argumento invalido. Caracter deve ser letra (a-z ou A-Z), digito (0-9), virgula, ponto ou espaco em branco.");
 
         if(i<(tam-1)){
-            if(((texto[i] == ',' || texto[i] == '.') && (texto[i+1] == ',' || texto[i+1] == '.'))||
-               (texto[i] == ' ' && !isalnum(texto[i+1])))
-                return false;
+            if((texto[i] == ',' || texto[i] == '.') && (texto[i+1] == ',' || texto[i+1] == '.'))
+                throw invalid_argument("Argumento Invalido. Ponto ou virgula nao pode ser seguido por virgula ou ponto.");
+            else if(texto[i] == ' ' && !isalnum(texto[i+1]))
+                throw invalid_argument("Argumento Invalido. Espaco em branco deve ser seguido por letra ou digito.");
         }
     }
-    return true;
 }
-bool Texto::setTexto(string texto){
-    if(!validar(texto)) return false;
+void Texto::setTexto(string texto){
+    validar(texto);
     this->texto = texto;
-    return true;
 }
 
 //Metodos-Data
@@ -100,7 +100,6 @@ bool Data::setAno(int ano){
         this->bissexto = false;
     }
     return true;
-
 }
 bool Data::setMes(int mes){
     if(mes > 12 || mes < 1){
@@ -150,11 +149,11 @@ bool Data::setData(string data) {
     this->data = data;
     return true;
 }
+
 //Metodos-Estado:
 bool Estado::validar(int escolha){
-    if(escolha < 1 || escolha > LIMITE){
-        return false;
-    }
+    if(escolha < 1 || escolha > LIMITE) return false;
+
     return true;
 }
 bool Estado::setEstado(int escolha){
@@ -170,15 +169,14 @@ bool Estado::setEstado(int escolha){
     }
     return true;
 }
-//Metodos-Prioridade:
 
-bool prioridade::validar(int valor){
-    if(valor < 1  valor > LIMITE){
-        return false;
-    }
+//Metodos-Prioridade:
+bool Prioridade::validar(int valor){
+    if(valor < 1 || valor > LIMITE) return false;
+
     return true;
 }
-bool prioridade::setPrio(int valor){
+bool Prioridade::setPrio(int valor){
     if(!validar(valor)){
         cout << "Escolha invalida" << endl;
         return false;
@@ -191,12 +189,11 @@ bool prioridade::setPrio(int valor){
     }
     return true;
 }
-//Metodos-Papel:
 
+//Metodos-Papel:
 bool Papel::validar(int valor){
-    if(valor < 1  valor > LIMITE){
-        return false;
-    }
+    if(valor < 1 || valor > LIMITE) return false;
+
     return true;
 }
 bool Papel::setEscolha_papel(int valor){
@@ -208,21 +205,23 @@ bool Papel::setEscolha_papel(int valor){
     switch(valor){
         case 1: this->escolha_papel = "DESENVOLVEDOR"; break;
         case 2: this->escolha_papel = "MESTRE SCRUM"; break;
-        case 3: this->escolhapapel = "PROPIETARIO DE PRODUTO"; break;
+        case 3: this->escolha_papel = "PROPIETARIO DE PRODUTO"; break;
     }
     return true;
 }
 
-//Metodos-Nome
+//Metodos-Nome:
 bool Nome::validar(string nome){
-    if(nome.size() > LIMITE) {return false;}
-    if((nome[0] == ' ') || (nome[10]== ' ')) cout <<"valor invalido";
-
+    if(nome.size() > LIMITE) return false;
+    if((nome[0] == ' ') || (nome[10]== ' ')){
+        cout <<"valor invalido";
+        return false;
+    }
     return true;
 }
 bool Nome::setNome(string nome){
-    if(!validar(nome)) {return false;
-    }
+    if(!validar(nome)) return false;
     this->nome = nome;
+
     return true;
 }
